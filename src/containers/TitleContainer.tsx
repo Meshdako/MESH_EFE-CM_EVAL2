1c9b0143006f97ae5bfdd3c8f97d196385fe3f0f
Meshdako <msobarzo@utem.cl>
2023-11-13 20:04:59 -0300

    UPLOAD: Agregado elementos visuales para visualizar correctamente las distintas vistas de la aplicación

--- src/containers/TitleContainer.tsx
+++ src/containers/TitleContainer.tsx
@@ -2,32 +2,30 @@ import React from 'react';
 import { Button, Image, Text, View, StyleSheet } from 'react-native';
 
 export default function TitleContainer() {
-
-    const onPressLearnMore = () => {
-        console.log('holaaa')
-    }
-
     return (
         <View style={styles.titleContainer}>
-            <Text style={styles.containerText}>MESH: Tarea Computación Movil</Text>
+            <Text style={styles.containerText}>
+                MESH: Tarea Computación Movil
+            </Text>
         </View>
-
-
     )
 }
 
 const styles = StyleSheet.create({
     titleContainer: {
         flex: 1,
+        width: '92.5%',
         backgroundColor: '#FF2400',
         alignItems: 'center',
         justifyContent: 'space-between',
     },
     containerText: {
+        backgroundColor: '#000',
+        fontStyle: 'italic',
         color: '#fff',
-        fontSize: 20,
-        margin: 50,
-        padding: 50,
+        fontSize: 15,
+        marginTop: 70,
+        padding: 20,
         fontWeight: 'bold',
         textAlign: 'center',
     },
