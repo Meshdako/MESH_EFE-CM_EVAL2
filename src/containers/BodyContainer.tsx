6bded62de996b355e8071c333ad8583be11300c4
Meshdako <msobarzo@utem.cl>
2023-11-13 20:06:39 -0300

    UPDATE: Agregada diferencia con el Título, para darle más espacio al cuerpo de la aplicación. Agregado el botón que fue creado, mostrando Ocultar Productos y Mostrar Productos

--- src/containers/BodyContainer.tsx
+++ src/containers/BodyContainer.tsx
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
-import { ScrollView, Text, StyleSheet } from 'react-native';
+import { ScrollView, Text, StyleSheet, View } from 'react-native';
 import { getFakestore } from '../services/fakestore.service';
 import { Data } from '../interfaces/fakestore.interface';
 import Products from '../components/products';
+import Button from '../components/button';
 
 export default function BodyContainer() {
-    
+    const [showProducts, setShowProducts] = useState(false);
+
+    const handlePress = () => {
+        setShowProducts(!showProducts);
+    };
+
     return (
-        <ScrollView style={styles.paraphContainer}>
-            <Products/>
-        </ScrollView >
+        <View style={styles.paraphContainer}>
+            <Button onPress={ handlePress } title={showProducts ? "Ocultar Productos" : "Mostrar Productos"}/>
+            {showProducts && <Products />}
+        </View >
     )
 }
 
 const styles = StyleSheet.create({
     paraphContainer: {
-        flex: 1,
-        backgroundColor: '#000',
+        flex: 3,
+        width: '92.3%',
+        backgroundColor: '#fff',
         padding:30,
     },
     paraph: {
