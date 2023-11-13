8e581a3440bb9b64e01c05ac7e7f899220553ac3
Meshdako <msobarzo@utem.cl>
2023-11-13 20:03:38 -0300

    UPDATE: Corregido mensaje de Error

--- src/services/fakestore.service.ts
+++ src/services/fakestore.service.ts
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 export const getFakestore = async () => {
     try {
+        console.log("Accediendo a la API de Fakestore");
+        
         const response =  await axios.get(`https://fakestoreapi.com/products`);
         return response.data;
     } catch (error) {
-        console.error(error);
+        console.error("Error al acceder a la API de Fakestore");
         throw new Error(`Error: ${error}`);
     }
 }
